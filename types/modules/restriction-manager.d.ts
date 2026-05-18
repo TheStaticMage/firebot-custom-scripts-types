a53fc545d954e3cbf645bfd5dd3b42c8598dd98d
The Static Mage <199814801+TheStaticMage@users.noreply.github.com>
2026-05-18 18:17:26 -0500

    Fix restriction-manager trigger

--- types/modules/restriction-manager.d.ts
+++ types/modules/restriction-manager.d.ts
@@ -1,12 +1,12 @@
 import EventEmitter from "events";
-import { RestrictionData } from "./command-manager";
 import { RestrictionType } from "../restrictions";
-import { Effects } from "@crowbartools/firebot-custom-scripts-types/types/effects";
+import { Trigger } from "../triggers";
+import { RestrictionData } from "./command-manager";
 
 export type RestrictionManager = EventEmitter & {
     registerRestriction: <RestrictionModel>(
         restriction: RestrictionType<RestrictionModel>
     ) => void;
     getRestrictionById: <RestrictionModel>(restrictionId: string) => RestrictionType<RestrictionModel> | undefined;
-    runRestrictionPredicates(triggerData: Effects.Trigger, restrictionData: RestrictionData, restrictionsAreInherited: boolean): Promise<void>;
+    runRestrictionPredicates(triggerData: Trigger, restrictionData: RestrictionData, restrictionsAreInherited: boolean): Promise<void>;
 };
